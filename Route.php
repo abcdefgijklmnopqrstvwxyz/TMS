c13f7b620de725d98cb3407606a00c7ce9f0f3ae
Umme Habiba <114835161+abcdefgijklmnopqrstvwxyz@users.noreply.github.com>
2024-07-12 15:34:27 +0500

    Add files via upload

---
+++ Route.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initi">
+    <title>Transport Services Management System</title>
+    <link rel="stylesheet"  href="repairing.css">
+    <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    
+    </head>
+
+    <body>
+        <div class="table">
+            <div class="table-header">
+                <p>Vehicle Deatils</p>
+                
+                
+            </div>
+            <div class="table-section">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Serial No</th>
+                            <th>Vehicle Name</th>
+                            <th>Vehicle Number</th>
+                            <th>Driver Name</th>
+                            <th>Starting point</th>
+                            <th>Ending point</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>01</td>
+                            <td>Van</td>
+                            <td>LEJ1245</td>
+                            <td>Nadeem Akhtar</td>
+                            <td>Narowal</td>
+                            <th>Lahore</th>
+                        </tr>
+
+                        <tr>
+                            <td>02</td>
+                            <td>Bus</td>
+                            <td>LEJ1201</td>
+                            <td>Bashir Ahmed</td>
+                            <td>Narowal</td>
+                            <th>Pasroor</th>
+                           
+                        </tr>
+
+                        <tr>
+                            <td>03</td>
+                            <td>Bus</td>
+                            <td>LEJ2871</td>
+                            <td>Muhammad Saddique</td>
+                            <td>Narowal</td>
+                            <th>Islamabad</th>
+                            
+                        </tr>
+
+                        <tr>
+                            <td>04</td>
+                            <td>Bus</td>
+                            <td>LEJ1245</td>
+                            <td>Akthar Ahmed</td>
+                            <td>Narowal</td>
+                            <th>Rawalpindi</th>
+                            
+                        </tr>
+
+                        
+                        
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
+    </body>
+    </html>
